6d2e08020443a9c8fda320e9c3528582f04d6e5b
AftonSlone <afton.slone@gmail.com>
2022-08-20 21:27:16 -0400

    Updated Auth

--- backend/src/controller/CheepController.ts
+++ backend/src/controller/CheepController.ts
@@ -1,6 +1,6 @@
-import { NextFunction, Request, Response } from 'express';
-import CheepRepository from '../repository/CheepRepository';
-import UserRepository from '../repository/UserRepository';
+import { NextFunction, Request, Response } from "express";
+import CheepRepository from "../repository/CheepRepository";
+import UserRepository from "../repository/UserRepository";
 
 export class CheepController {
   async all(request: Request, response: Response, next: NextFunction) {
@@ -24,12 +24,8 @@ export class CheepController {
   }
 
   async save(request: Request, response: Response, next: NextFunction) {
-    const user = await UserRepository.findOneBy({
-      id: request.body.userID,
-    });
-
     const newCheep = {
-      user: user,
+      user: request.body.decoded,
       content: request.body.content,
     };
     return CheepRepository.save(newCheep);
@@ -39,7 +35,7 @@ export class CheepController {
     const cheepToRemove = await CheepRepository.findOneBy({
       id: parseInt(request.params.id),
     });
-    if (cheepToRemove) throw new Error('This Cheep was not found');
+    if (cheepToRemove) throw new Error("This Cheep was not found");
     await CheepRepository.remove(cheepToRemove);
   }
 }
