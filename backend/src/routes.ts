33e1468ff589b4d240e6139abce6cdee2d44a624
AftonSlone <afton.slone@gmail.com>
2022-09-07 15:02:39 -0400

    Updates to routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
+import { cheepRouter } from "./controllers/CheepController";
+import { cheepPhotoRouter } from "./controllers/CheepPhotoController";
+import { likesRouter } from "./controllers/LikesController";
 import { loginRouter } from "./controllers/LoginController";
+import { recheepRouter } from "./controllers/RecheepController";
 import { userRouter } from "./controllers/UserController";
 
 export const router = Router();
 
 router.use("/users", userRouter);
 router.use("/login", loginRouter);
+router.use("/cheeps", cheepRouter);
+router.use("/likes", likesRouter);
+router.use("/recheeps", recheepRouter);
+router.use("/cheepphotos", cheepPhotoRouter);
