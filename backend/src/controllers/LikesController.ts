440d2075a6b6f31e564215e2bf8dfb96b5f7a933
AftonSlone <afton.slone@gmail.com>
2022-09-07 14:58:30 -0400

    Updates to CheepPhoto, Likes, and Recheep controllers

--- backend/src/controllers/LikesController.ts
+++ backend/src/controllers/LikesController.ts
@@ -1,1 +1,58 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { verifyToken } from "../customMiddleware/Authorization";
+import { LikesService } from "../services/likesService";
 
+export const likesRouter = Router();
+const likesService = new LikesService();
+
+likesRouter.get(
+  "/",
+  verifyToken,
+  async (request: Request, responce: Response, next: NextFunction) => {
+    try {
+      const results = await likesService.all(request, responce, next);
+      responce.json(results);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+likesRouter.get(
+  "/:id",
+  verifyToken,
+  async (request: Request, responce: Response, next: NextFunction) => {
+    try {
+      const results = await likesService.one(request, responce, next);
+      responce.json(results);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+likesRouter.post(
+  "/",
+  verifyToken,
+  async (request: Request, responce: Response, next: NextFunction) => {
+    try {
+      const results = await likesService.save(request, responce, next);
+      responce.json(results);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+likesRouter.delete(
+  "/:id",
+  verifyToken,
+  async (request: Request, responce: Response, next: NextFunction) => {
+    try {
+      const results = await likesService.remove(request, responce, next);
+      responce.json(results);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
--- backend/src/controllers/RecheepController.ts
+++ backend/src/controllers/RecheepController.ts
@@ -1,1 +1,58 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { verifyToken } from "../customMiddleware/Authorization";
+import { RecheepService } from "../services/reCheepService";
 
+export const recheepRouter = Router();
+const recheepService = new RecheepService();
+
+recheepRouter.get(
+  "/",
+  verifyToken,
+  async (request: Request, responce: Response, next: NextFunction) => {
+    try {
+      const results = await recheepService.all(request, responce, next);
+      responce.json(results);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+recheepRouter.get(
+  "/:id",
+  verifyToken,
+  async (request: Request, responce: Response, next: NextFunction) => {
+    try {
+      const results = await recheepService.one(request, responce, next);
+      responce.json(results);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+recheepRouter.post(
+  "/",
+  verifyToken,
+  async (request: Request, responce: Response, next: NextFunction) => {
+    try {
+      const results = await recheepService.save(request, responce, next);
+      responce.json(results);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+recheepRouter.delete(
+  "/:id",
+  verifyToken,
+  async (request: Request, responce: Response, next: NextFunction) => {
+    try {
+      const results = await recheepService.remove(request, responce, next);
+      responce.json(results);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
